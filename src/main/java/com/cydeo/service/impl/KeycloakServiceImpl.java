11b19a3ee185683ab2135d6928ad6814309b8f7c
Ivan_Cydeo <remezivan1@gmail.com>
2023-06-09 12:31:14 -0400

    Keycloak - Auto user create done

---
+++ src/main/java/com/cydeo/service/impl/KeycloakServiceImpl.java
@@ -0,0 +1,101 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.config.KeycloakProperties;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.KeycloakService;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.stereotype.Service;
+
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.keycloak.admin.client.CreatedResponseUtil.getCreatedId;
+
+@Service
+public class KeycloakServiceImpl implements KeycloakService {
+
+
+    private final KeycloakProperties keycloakProperties;
+
+    public KeycloakServiceImpl(KeycloakProperties keycloakProperties) {
+
+        this.keycloakProperties = keycloakProperties;
+    }
+
+    @Override
+    public Response userCreate(UserDTO userDTO) {
+
+        CredentialRepresentation credential = new CredentialRepresentation();
+        credential.setType(CredentialRepresentation.PASSWORD);
+        credential.setTemporary(false);
+        credential.setValue(userDTO.getPassWord());
+    // ^^ captures PW from Postman and assigns it to User PW in Keycloak
+
+        UserRepresentation keycloakUser = new UserRepresentation();
+        keycloakUser.setUsername(userDTO.getUserName());
+        keycloakUser.setFirstName(userDTO.getFirstName());
+        keycloakUser.setLastName(userDTO.getLastName());
+        keycloakUser.setEmail(userDTO.getUserName());
+        keycloakUser.setCredentials(asList(credential));
+        keycloakUser.setEmailVerified(true);
+        keycloakUser.setEnabled(true);
+    // ^^ basically acts as a MAPPER b/w UserDTO (User create form) and
+        // Keycloak User creation
+
+        Keycloak keycloak = getKeycloakInstance();
+    // ^^ in order to do anything in Keycloak, we need to open an Instance
+        // same as opening a Transaction in DB to modify data
+
+        RealmResource realmResource = keycloak.realm(keycloakProperties.getRealm());
+        UsersResource usersResource = realmResource.users();
+
+        // Create Keycloak user
+        Response result = usersResource.create(keycloakUser);
+
+        String userId = getCreatedId(result);
+        // ^^ gets User ID created by Keycloak
+        ClientRepresentation appClient = realmResource.clients()
+                .findByClientId(keycloakProperties.getClientId()).get(0);
+
+        RoleRepresentation userClientRole = realmResource.clients().get(appClient.getId()) //
+                .roles().get(userDTO.getRole().getDescription()).toRepresentation();
+        // ^^ goes to Keycloak Realm and matches User Role w/ standard roles in the Realm
+        // will throw error if Role doesn't exist in the Realm
+
+        realmResource.users().get(userId).roles().clientLevel(appClient.getId())
+                .add(List.of(userClientRole));
+
+        keycloak.close();
+        return result;
+    }
+
+    @Override
+    public void delete(String userName) {
+
+        Keycloak keycloak = getKeycloakInstance();
+
+        RealmResource realmResource = keycloak.realm(keycloakProperties.getRealm());
+        UsersResource usersResource = realmResource.users();
+
+        List<UserRepresentation> userRepresentations = usersResource.search(userName);
+        String uid = userRepresentations.get(0).getId();
+        usersResource.delete(uid);
+
+        keycloak.close();
+    }
+
+    private Keycloak getKeycloakInstance(){
+        return Keycloak.getInstance(keycloakProperties.getAuthServerUrl(),
+                keycloakProperties.getMasterRealm(), keycloakProperties.getMasterUser()
+                , keycloakProperties.getMasterUserPswd(), keycloakProperties.getMasterClient());
+    }
+    // ^^ using KeycloakProperties to pass serverURL, Realm, Master username + PW, clientID
+    // from application.properties to Spring, so it can access Keycloak
+}
